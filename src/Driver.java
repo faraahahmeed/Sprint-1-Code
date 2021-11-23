ea45bbe056498768eaded225c8d99a9680c535f0
msherifff <marimsherif2008@yahoo.com>
2021-11-23 11:22:57 +0200

    AdminUser

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,51 @@
-public class Driver {
+public class Driver extends User {
+    private String nationalid;
+    private String drivinglicense;
+    private String driverid;
+    private float ridecost;
+    private String area;
+    private float price;
+
+    public void setNationalid(String nationalid) {
+        this.nationalid = nationalid;
+    }
+    public void setDrivinglicense(String drivinglicense) {
+        this.drivinglicense = drivinglicense;
+    }
+    public void setDriverid(String driverid) {
+        this.driverid = driverid;
+    }
+    public String getNationalid() {
+        return nationalid;
+    }
+    public String getDrivinglicense() {
+        return drivinglicense;
+    }
+    public String getDriverid() {
+        return driverid;
+    }
+    public void Balance(float ridecost)
+    {
+        this.ridecost = ridecost;
+    }
+    public void FavoriteArea(String Area)
+    {
+        this.area = Area;
+    }
+    //public abstract void Register(String w, String x, String y, String z);
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    public boolean endride(){
+
+    }
+    public Object listride(){
+
+    }
 }
+
